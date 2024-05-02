ba31347d6194c3fbff001664c4be0e077993542c
MesfinYitbarek <mesfinyitbarek55@gmail.com>
2024-05-02 14:07:39 +0300

    connceted to dsatabase

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express, { request } from "express";
 import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
---
+++ frontend/src/components/Home/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>The Future of <span>Online Learning</span>is Here.</h1>
+        <button>Explore More !</button>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
