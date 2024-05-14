adf08dd964032be6d292f84f12ec69d8a407529a
justsoul12 <140943417+MesfinYitbarek@users.noreply.github.com>
2024-05-14 17:48:03 +0300

    complete functionalty for profile update

--- frontend/src/components/Profile/updateProfile.jsx
+++ frontend/src/components/Profile/updateProfile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Header from "../Common/Header";
 import { useSelector } from "react-redux";
 import { useRef } from "react";
 import {
@@ -7,7 +8,14 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import { app } from '../../../firebase'
+import { app } from "../../../firebase";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+
 const UpdateProfile = () => {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
@@ -15,8 +23,9 @@ const UpdateProfile = () => {
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  
+  const dispatch = useDispatch();
 
+  console.log(formData);
   useEffect(() => {
     if (file) {
       hanadleFileUpload(file);
@@ -30,7 +39,7 @@ const UpdateProfile = () => {
     const uploadTask = uploadBytesResumable(storageRef, file);
 
     uploadTask.on(
-      "state.changed",
+      "state_changed",
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -46,42 +55,96 @@ const UpdateProfile = () => {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div>
-      <h1>Profile</h1>
-      <form action="">
-        <img src={ formData.avatar || currentUser.avatar} alt="profile" />
-        <p>
-            {
-                fileUploadError ? (
-                    <span>
-                        Error image upload( image must be less than 2 mb)
-                    </span>
-                ) : filePerc > 0 && filePerc < 100 ? (
-                    <span>{`Uploading ${filePerc}%`}</span>
-                ) : filePerc === 100 ? (
-                    <span>Image successfully uploaded!</span>
-                ) : (
-                    ''
-                )
-            }
-        </p>
-        <input
-          onChange={(e) => setFile(e.target.files[0])}
-          type="file"
-          ref={fileRef}
-         
-          accept="image/*"
-        />
-        <button onClick={() => fileRef.current.click()}>Change Photo</button>
-        <input type="text" placeholder="Username" id="username" />
-        <input type="email" placeholder="Email" id="email" />
-        <input type="text" placeholder="Password" id="password" />
-        <button>Update</button>
-      </form>
-      <div>
-        <span>Delete account</span>
-        <span>Log Out</span>
+      <Header />
+      <div className=" p-20">
+        <div className=" mb-5 sm:mb-8 ">
+          <h1 className="text-3xl font-semibold">Personal details</h1>
+          <p className=" text-xl">
+            Add your personal details as you would like to appear on your
+            profile.
+          </p>
+        </div>
+
+        <form onSubmit={handleSubmit} action="">
+          <input
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            ref={fileRef}
+            accept="image/*"
+            hidden
+          />
+          <img
+            src={formData.avatar || currentUser.avatar}
+            alt="profile"
+            className=" h-36 w-36 border-4 border-double border-slate-200 rounded-full"
+          />
+          <p>
+            {fileUploadError ? (
+              <span>Error image upload( image must be less than 2 mb)</span>
+            ) : filePerc > 0 && filePerc < 100 ? (
+              <span>{`Uploading ${filePerc}%`}</span>
+            ) : filePerc === 100 ? (
+              <span>Image successfully uploaded!</span>
+            ) : (
+              ""
+            )}
+          </p>
+
+          <button onClick={() => fileRef.current.click()}>Change Photo</button>
+          <input
+            type="text"
+            defaultValue={currentUser.username}
+            placeholder="Username"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            defaultValue={currentUser.email}
+            placeholder="Email"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={handleChange}
+          />
+          <button>Update</button>
+        </form>
+        <div>
+          <span>Delete account</span>
+          <span>Log Out</span>
+        </div>
       </div>
     </div>
   );
